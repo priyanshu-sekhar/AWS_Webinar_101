4b9beef1f111e6610261037204060057b6c0b443
Priyanshu Sekhar <pspatra@amazon.com>
2020-01-19 14:29:06 +0530

    added alert_custom

--- apis/alert_neighborhood.py
+++ apis/alert_neighborhood.py
@@ -1,9 +1,21 @@
 from utils import SNSCommonUtils
 
 
+def alert_custom(event, context):
+    record = event['Records'][0]
+    if record['eventName'] == 'INSERT':
+        name = record['dynamodb']['NewImage']['NAME']['S']
+        SNSCommonUtils.publish_to_topic(
+            topic_arn='<topic_arn>',
+            message='A new member in you neighborhood. Say Hi to ' + name,
+            subject='Say Hi to your new neighbor - ' + name
+        )
+
+
 def alert(event, context):
     SNSCommonUtils.publish_to_topic(
-        topic_arn='arn:aws:sns:us-west-2:553658736773:neighborhood_1',
-        message='A new Member in you neighborhood. Say Hi',
+        topic_arn='<topic_arn>',
+        message='A new member in you neighborhood. Say Hi',
         subject='Say Hi to your new neighbor'
     )
+
